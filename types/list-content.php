6a05cbf16f722dbbb55f07c41965b502b86f60fc
Henning Leutz <leutz@pcsg.de>
2015-02-20 12:12:09 +0100

    content list

--- types/list-content.php
+++ types/list-content.php
@@ -1,36 +1,16 @@
 <?php
 
-/**
- * Blog List
- */
 
-$start = 0;
-$max   = $Site->getAttribute( 'quiqqer.settings.sitetypes.list.max' );
-
-if ( !$max ) {
-    $max = 5;
-}
-
-if ( isset( $_REQUEST['sheet'] ) ) {
-    $start = ( (int)$_REQUEST['sheet'] - 1 ) * $max;
-}
-
-$count_children = $Site->getChildren(array(
-    'count'	=> 'count'
-));
-
-if ( is_array( $count_children ) ) {
-    $count_children = count( $count_children );
-}
-
-// sheets
-$sheets = ceil( $count_children / $max );
-
-$children = $Site->getChildren(array(
-    'limit' => $start .','. $max
+$ChildrenList = new \QUI\Controls\ChildrenList(array(
+    'Site'        => $Site,
+    'limit'       => $Site->getAttribute( 'quiqqer.settings.sitetypes.list.max' ),
+    'showSheets'  => $Site->getAttribute( 'quiqqer.settings.sitetypes.list.showSheets' ),
+    'showImages'  => $Site->getAttribute( 'quiqqer.settings.sitetypes.list.showImages' ),
+    'showShort'   => false,
+    'showHeader'  => $Site->getAttribute( 'quiqqer.settings.sitetypes.list.showHeader' ),
+    'showContent' => true
 ));
 
 $Engine->assign(array(
-    'sheets'   => $sheets,
-    'children' => $children
+    'ChildrenList' => $ChildrenList
 ));
